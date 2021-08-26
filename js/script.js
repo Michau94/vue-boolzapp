30fbe049af4b7dce6b6fb9f402f3d30280cdd663
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-08-26 16:02:34 +0200

    dayjs + format

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@ console.log('js ok');
 console.log(user);
 console.log(contacts);
 
+dayjs.extend(dayjs_plugin_customParseFormat);
+
 
 const root = new Vue({
     el: "#root",
     data: {
         user,
-        contacts
+        contacts,
+        search: '',
+        message: '',
+        today: dayjs()
+
 
     },
     methods: {}
