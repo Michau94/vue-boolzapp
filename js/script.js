dfcd59130e9c8e8bffed5dfafccc03aac25e4c0f
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-08-26 15:46:33 +0200

    v-for contatti e utente principale

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('js ok');
+console.log(user);
+console.log(contacts);
+
+
+const root = new Vue({
+    el: "#root",
+    data: {
+        user,
+        contacts
+
+    },
+    methods: {}
+})
