3e5a95ba48add4cf593a882c354779711ebef96d
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-08-26 17:44:39 +0200

    current user @click

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ console.log(user);
 console.log(contacts);
 
 dayjs.extend(dayjs_plugin_customParseFormat);
+const mainChat = document.getElementById('mainChat');
 
 
 const root = new Vue({
@@ -12,7 +13,10 @@ const root = new Vue({
         contacts,
         search: '',
         message: '',
-        today: dayjs()
+        today: dayjs(),
+        currentUser: 'Michele',
+        currentAvatar: '_1'
+
 
 
     },
@@ -26,6 +30,13 @@ const root = new Vue({
             }
 
             return updName.includes(this.search.trim().toLowerCase()) ? true : false;
+        },
+
+        actualUser(user) {
+            this.currentUser = user.name;
+            this.currentAvatar = user.avatar;
+
         }
+
     }
 })
