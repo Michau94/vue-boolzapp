1571395fe026e4f63567d14ec4a62e0eabcdf0e2
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-08-27 13:12:25 +0200

    send message + reply "ok" in 2 sec

--- js/script.js
+++ js/script.js
@@ -38,8 +38,32 @@ const root = new Vue({
             this.currentIndex = index;
 
         },
+        newMessage(ind) {
 
+            let newMessage = {
+                date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+                message: this.message,
+                status: 'sent'
 
+            };
 
+            this.contacts[ind].messages.push(newMessage);
+
+            this.message = " ";
+
+            setTimeout(() => {
+
+                let replyMessage = {
+                    date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
+                    message: 'OK',
+                    status: 'received'
+
+                };
+
+                this.contacts[ind].messages.push(replyMessage);
+            }, 2000)
+
+        }
     }
+
 })
